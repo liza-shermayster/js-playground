636ccef35cf762859b716d926a514e04546f69e8
Liza Shermayster <lizashermayster@Lizas-MacBook-Pro.local>
2020-03-18 14:25:51 +0200

    random+floot

---
+++ src/math_random_floor.js
@@ -0,0 +1,10 @@
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+console.log(randomNumberBetween0and19);
+
+function randomWholeNum() {
+  let test = Math.floor(Math.random() * 10) + 0;
+  return test;
+}
+console.log(randomWholeNum());
+
